1e9ac09e95f1cb5dda0b35cd7116b7e65389e84a
jonesarc <71904973+jonesarc@users.noreply.github.com>
2021-01-19 22:24:14 -0600

    Add files via upload

--- gcxstreamlit.py
+++ gcxstreamlit.py
@@ -13,13 +13,22 @@ import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
 
-st.title('GCxGC by ARC')
+st.title('GCxGC by [ARC] (https://www.activatedresearch.com/)')
 
 st.write('This program will convert 1D GC data into a 2D GCxGC heatmap')
 
-uploaded_file = st.sidebar.file_uploader('Upload your GCxGC data file here')
+uploaded_file = st.sidebar.file_uploader('Upload your csv data file here or select an example from the drop down below')
 
-if uploaded_file is not None:
+option = st.sidebar.selectbox('Examples:', ('Select','Gasoline','Biodiesel'))
+
+if uploaded_file is not None or option != 'Select':
+    if option == 'Gasoline':
+        uploaded_file = open("Gasoline.csv")
+    elif option == 'Biodiesel':
+        uploaded_file = open("Biodiesel.csv")
+    elif option == 'Spearmint':
+        uploaded_file = open("Spearmint.csv")
+
     df = pd.read_csv(uploaded_file,low_memory=False)
     dn = df.drop([0,1])
     dn = dn.to_numpy(dtype='float64')
@@ -33,11 +42,18 @@ if uploaded_file is not None:
     rate = round(numpoints/runtime/60)
     st.sidebar.text('Acquisition rate is ' + str(rate) + ' Hz')
 
-    modtime = st.sidebar.number_input('Enter modulation period in seconds')
-    if not modtime:
-        st.sidebar.warning('Please input the modulation period')
-        st.stop()
-    st.sidebar.success('Modulation time recorded')
+    if option == 'Select':
+        modtime = st.sidebar.number_input('Enter modulation period in seconds')
+        if not modtime:
+            st.sidebar.warning('Please input the modulation period')
+            st.stop()
+        st.sidebar.success('Modulation time recorded')
+    elif option == 'Gasoline':
+        modtime = 3.0
+    elif option == 'Biodiesel':
+        modtime = 3.3
+    elif option == 'Spearmint':
+        modtime = 2.1
 
     ## Pad matrix so it can be resized
     xp = int(round(modtime*rate))
@@ -52,9 +68,8 @@ if uploaded_file is not None:
     data_2D = np.transpose(data_2D)
     st.sidebar.text('A ' + str(xp) + ' x ' + str(yp) + ' array was created')
 
-    power = st.sidebar.slider('Select level of enhancement',1,100,1)
-    colorscale_select = st.sidebar.selectbox('Colorscale',('rainbow', 'jet', 'inferno', 'thermal',
-                                                           'portland','spectral', 'balance', 'edge', 'hsv'))
+    power = st.sidebar.slider('Select level of enhancement',1,100,10)
+    colorscale_select = st.sidebar.selectbox('Colorscale',('rainbow', 'jet', 'inferno', 'thermal', 'portland','spectral', 'balance', 'edge', 'hsv'))
     scheck = st.sidebar.checkbox('Use smoothing?',value=True)
     if scheck:
         smoothing = 'best'
@@ -63,22 +78,29 @@ if uploaded_file is not None:
 
     data_2D_power = np.power(data_2D,1/(power))
 
-###### Plotting
+###### Plotting, see https://plotly.com/python-api-reference/generated/plotly.graph_objects.Heatmap.html
     x_1D = np.arange(start=0,stop=runtime,step=runtime/len(data_2D[0,:]))
     y_2D = np.arange(start=0,stop=modtime,step=modtime/len(data_2D[:,0]))
-    fig = go.Figure(data=go.Heatmap(x=x_1D,y=y_2D,z=data_2D_power, zsmooth=smoothing,
-                                    colorscale=colorscale_select))
-        #https://plotly.com/python-api-reference/generated/plotly.graph_objects.Heatmap.html
-#add cache, check out zmid
+    fig = go.Figure(data=go.Heatmap(x=x_1D,y=y_2D,z=data_2D_power, zsmooth=smoothing, colorscale=colorscale_select))
     fig.update_layout(title='2D heatmap:',xaxis_title='1D time (min)',yaxis_title='2D time (s)',hovermode="x unified")
     st.plotly_chart(fig)
 
     slicetime = st.number_input('Enter time (min) for extracted chromatogram',min_value=0.0,max_value=runtime,value=0.0)
     arrayslice = int(round(slicetime/runtime*len(data_2D[0,:])))
-    fig2 = px.line(x=y_2D,y=data_2D[:,arrayslice],labels={'x':'2D time (s)', 'y':'FID signal (pA*s)'},
-                   title='Extracted 2D chromatogram:')
+    fig2 = px.line(x=y_2D,y=data_2D[:,arrayslice],labels={'x':'2D time (s)', 'y':'FID signal (pA*s)'}, title='Extracted 2D chromatogram:')
+    #fig2 = px.line(x=y_2D,y=[data_2D[:,(arrayslice-1)],data_2D[:,arrayslice],data_2D[:,(arrayslice+1)]],labels={'x':'2D time (s)', 'y':'FID signal (pA*s)'}, title='Extracted 2D chromatogram:')
     st.plotly_chart(fig2)
                 
-    fig3 = px.line(x=x_1D,y=np.sum(data_2D,axis=0),render_mode='svg',line_shape='spline',labels={'x':'1D time (min)', 'y':'FID signal (pA*s)'},
-                   title='Collapsed 1D chromatogram:')
+    fig3 = px.line(x=x_1D,y=np.sum(data_2D,axis=0),render_mode='svg',line_shape='spline',labels={'x':'1D time (min)', 'y':'FID signal (pA*s)'},title='Projected 1D chromatogram:')
+    fig3.add_shape(type="line",x0=slicetime,y0=0,x1=slicetime,y1=max(np.sum(data_2D,axis=0)),line=dict(color="Red",width=1)) #add line to show where extracted chromatogram is
     st.plotly_chart(fig3)
+
+else:
+    st.write("Data files must have 3 rows of header data and then time and signal in the first two columns. Here is an example taken from an Agilent 7890 GC, by exporting the .ch file to a csv with MSD ChemStation:")
+    #st.write(f'<a href="data:file/csv;" download="https://github.com/activated-research-company/gcxgc/raw/main/Gasoline.CSV">Download csv file</a>',unsafe_allow_html=True)
+    #st.write('[Download csv file] (https://github.com/activated-research-company/gcxgc/raw/main/Gasoline.CSV)')
+    #<a href="https://github.com/activated-research-company/gcxgc/raw/main/Gasoline.CSV" download></a>
+    filelink = '<a href="https://github.com/activated-research-company/gcxgc/raw/main/Gasoline.CSV" download="Gasoline.csv">Download csv file</a>'
+    st.write(filelink,unsafe_allow_html=True)
+    st.image("https://www.activatedresearch.com/wp-content/uploads/2021/01/Example.png", use_column_width=True)
+    
